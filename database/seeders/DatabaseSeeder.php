f085fbbba081bf9b81bf22cade7a0d2e410c2b65
timur <mukhamedievtimur@gmail.com>
2025-10-18 17:22:32 +0500

    cидер добавление роли и админа

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $managerRole = Role::firstOrCreate(['name' => 'manager']);
+
+        $manager = User::firstOrCreate(
+            ['email' => 'test@test.com'],
+            ['name' => 'admin', 'password' => bcrypt('password')]
+        );
+        $manager->assignRole($managerRole);
     }
 }
